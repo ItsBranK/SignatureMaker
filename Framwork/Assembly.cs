9cb3bc2c12ac4b307854621877f26a5911390549
BranK <23649425+ItsBranK@users.noreply.github.com>
2024-12-20 04:38:34 -0500

    v2.8
    - Upgraded the project from `.NET 8.0` to `.NET 9.0`.

--- Framwork/Assembly.cs
+++ Framwork/Assembly.cs
@@ -6,7 +6,7 @@ namespace SignatureMaker.Framework
     {
         private static readonly bool m_x64Bit = true;
         private static readonly string m_title = "ItsBranK's Signature Maker";
-        private static readonly string m_version = "2.7";
+        private static readonly string m_version = "2.8";
 
         public static bool Is64Bit() { return m_x64Bit; }
         public static string GetTitle() { return (m_title + (Is64Bit() ? " (x64)" : " (x86)")); }
